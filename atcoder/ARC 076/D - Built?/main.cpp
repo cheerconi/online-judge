91c713ca6b0d322a14afbe0c8bdf9145721535e5
Carl <cheerconi@163.com>
2017-06-30 17:50:26 +0800

    add Built?

--- atcoder/ARC 076/D - Built?/main.cpp
+++ atcoder/ARC 076/D - Built?/main.cpp
@@ -1,146 +1,94 @@
 #include <iostream>
-#include <set>
-#include <algorithm>
 #include <vector>
+#include <algorithm>
 #include <map>
+using namespace std;
 
 typedef long long LL;
 
-using namespace std;
-
-struct town{
+struct Point {
     int x;
     int y;
     int id;
-
-    town(int _x, int _y, int _id) {
+    Point(int _x, int _y) {
         x = _x;
         y = _y;
-        id = _id;
     }
 };
 
-int find(vector<int> &pre, int i);
-void add(vector<int> &pre, int i, int j);
-vector<int> init(int n);
-
-
-//初始化并查集, 每个祖先节点满足 pre[i] = i
-// 输入 n 为节点个数
-// 返回 pre， 每个节点都是自己的祖先
-vector<int> init(int n) {
-    vector<int> pre(n, 0);
-    for (int i = 1; i < n; i++) pre[i] = i;
-    return pre;
+static bool cmpx(Point const & p1, Point const & p2) {
+    return p1.x < p2.x;
+}
+static bool cmpy(Point const & p1, Point const & p2) {
+    return p1.y < p2.y;
 }
 
-// 插入 (i,j) 的边， 即改变 pre，使得 i, j 有相同的祖先
-// 输入 i,j
-// 改变 pre， 无返回
-void add(vector<int> &pre, int i, int j) {
-    int tmpi = find(pre, i);
-    int tmpj = find(pre, j);
-    pre[tmpi] = tmpj;
-    return;
-
-}
-
-// 寻找 i 的祖先， 中途会递归优化， 使得下次能够更快地查询
-// 输入 pre, 需要查询的节点 i
-// 返回 i 的祖先, 并会改变 pre
-int find(vector<int> &pre, int i) {
-    if (pre[i] == i) return i;
-    int tmp = find(pre, pre[i]);
-    pre[i] = tmp;
-    return tmp;
-
-}
-
-static bool cmpx(town a, town b) {
-    return a.x < b.x;
-}
-static bool cmpy(town a, town b) {
-    return a.y < b.y;
-}
+struct Unionset {
+    vector<int> parents;
+    Unionset(int n) {
+        parents = vector<int>(n, -1);
+        for (int i = 0; i < n; i++) parents[i] = i;
+    }
+    int find(int i) {
+        if (parents[i] == i) return i;
+        int ret = find(parents[i]);
+        parents[i] = ret;
+        return ret;
+    }
+    void add(int i, int j) {
+        int dadi = find(i);
+        int dadj = find(j);
+        if (dadi == dadj) return;
+        parents[dadi] = dadj;
+        return;
+    }
+    bool check(int i, int j) {
+        int dadi = find(i);
+        int dadj = find(j);
+        return dadi==dadj;
+    }
+};
 
 int main() {
-    int N;
-    scanf("%d", &N);
-    vector<int> pre = init(N);
+//    freopen("test.txt", "r", stdin);
+    int n;
+    scanf("%d", &n);
+    vector<Point> towns(n, Point(0, 0));
+    for (int i = 0; i < n; i++) {
+        scanf("%d%d", &towns[i].x, &towns[i].y);
+        towns[i].id = i;
+    }
+    vector<Point> townx = towns;
+    vector<Point> towny = towns;
 
-    vector<town> towns(N, town(0, 0, -1));
-    for (int n = 0; n < N; n++) {
-        scanf("%d%d", &towns[n].x, &towns[n].y);
-        towns[n].id = n;
-    }
-    sort(towns.begin(), towns.end(), cmpx);
-    multimap<int, pair<int, int> > mapx;
-    for (int n = 1; n < N; n++) {
-        if (towns[n].x - towns[n-1].x == 0) {
-            add(pre, n, n-1);
-            continue;
-        }
-        mapx.insert({towns[n].x-towns[n-1].x, {towns[n].id, towns[n-1].id}});
-    }
+    sort(townx.begin(), townx.end(), cmpx);
+    sort(towny.begin(), towny.end(), cmpy);
 
-    sort(towns.begin(), towns.end(), cmpy);
-    multimap<int, pair<int, int> > mapy;
-    for (int n = 1; n < N; n++) {
-        if (towns[n].y - towns[n-1].y == 0) {
-            add(pre, n, n-1);
-            continue;
-        }
-        mapy.insert({towns[n].y-towns[n-1].y, {towns[n].id, towns[n-1].id}});
+
+    multimap<int, pair<int, int> > graph;
+
+    for (int i = 1; i < n; i++) {
+        graph.insert({townx[i].x - townx[i-1].x, {townx[i].id, townx[i-1].id}});
+        graph.insert({towny[i].y - towny[i-1].y, {towny[i].id, towny[i-1].id}});
     }
+    Unionset connect = Unionset(n);
+
     LL cost = 0;
-    int dx, dy, id1,id2;
-    while (mapy.size()>0 && mapx.size() > 0) {
-        dx = (mapx.begin())->first;
-        dy = (mapy.begin())->first;
 
-        if (dx > dy) {
-            id1 = mapy.begin()->second.first;
-            id2 = mapy.begin()->second.second;
-            if (find(pre, id1) != find(pre, id2)) {
-                add(pre, id1, id2);
-                cost += dy;
-            }
-            mapy.erase(mapy.begin());
-
-            continue;
+    for (auto it = graph.begin(); it != graph.end(); it++) {
+        int id1 = it->second.first;
+        int id2 = it->second.second;
+        if (it->first == 0) {
+            connect.add(id1, id2);
         }
-        id1 = mapx.begin()->second.first;
-        id2 = mapx.begin()->second.second;
-        if (find(pre, id1) != find(pre, id2)) {
-            cost +=  dx;
-            add(pre, id1, id2);
+        else if (!connect.check(id1, id2)) {
+            cost += it->first;
+            connect.add(id1, id2);
         }
-        mapx.erase(mapx.begin());
-    }
-    while (mapy.size() > 0) {
-        dx = mapx.begin()->first;
-        id1 = mapy.begin()->second.first;
-        id2 = mapy.begin()->second.second;
-        if (find(pre, id1) != find(pre, id2)) {
-            add(pre, id1, id2);
-            cost += dx;
-        }
-        mapy.erase(mapy.begin());
-    }
-    while (mapx.size() > 0) {
-        dx = mapx.begin()->first;
-        id1 = mapx.begin()->second.first;
-        id2 = mapx.begin()->second.second;
-        if (find(pre, id1) != find(pre, id2)) {
-            add(pre, id1, id2);
-            cost += dx;
-        }
-        mapx.erase(mapx.begin());
     }
     cout << cost << endl;
 
-
-
-
     return 0;
+
+
 }

ecc61dd1f895f45e59805be1b8b76eb48ab7e07f
Carl <cheerconi@163.com>
2017-07-03 22:15:39 +0800

    add guruguru

--- atcoder/ARC 077/E - guruguru/main.cpp
+++ atcoder/ARC 077/E - guruguru/main.cpp
@@ -3,41 +3,43 @@
 #include <algorithm>
 
 using namespace std;
+typedef long long LL;
 
 
 int main(){
 //    freopen("test.txt", "r", stdin);
-    int n,m, pre, now;
-    scanf("%d%d", &n, &m);
-    vector<int> save(m+1, 0);
-    scanf("%d", &pre);
-    pre = pre % m;
-    int cnt = 0;
-    for (int i = 1; i <= n; i++) {
-        scanf("%d", &now);
-        now = now % m;
-        if (now < pre) now += m;
-        int dis = now - pre;
-        cnt += dis;
-        if (pre+2 <= now) {
-            if (now < m) {
-                save[pre+2] += 1;
-                save[now+1] -= 1;
-            }
-            else if (pre+2 >= m) {
-                save[pre+2-m] += 1;
-                save[now+1-m] -= 1;
-            }
-            else {
-                save[pre+2] += 1;
-                save[m] -= 1;
-                save[0] += 1;
-                save[now+1-m] -= 1;
-            }
+    LL n, m, pre, now;
+    scanf("%lld%lld%lld", &n, &m, &pre);
+    vector<LL> a(m+2, 0);
+    vector<LL> b(m+2, 0);
+    for (int i = 1; i < n; i++) {
+        scanf("%lld", &now);
+        if (now > pre) {
+            b[1] += now - pre;
+            b[pre + 1] -= now - pre;
+            b[now + 1] += now - pre;
+            a[pre+1] -= 1;
+            a[now+1] += 1;
+            b[pre + 1] += now + 1;
+            b[now + 1] -= now + 1;
         }
-        pre = now % m;
+        else { // now < pre
+            b[now + 1] += now - pre + m;
+            b[pre + 1] -= now - pre + m;
+            a[1] -= 1;
+            a[now + 1] += 1;
+            b[1] += now + 1;
+            b[now + 1] -= now + 1;
+            a[pre + 1] -= 1;
+            b[pre + 1] += now + m + 1;
+        }
+        pre = now;
     }
-    m = 0;
-    for (int i = 0; i < m; i++) save[]
-    cout << cnt - m << endl;
+    LL ret = a[1] + b[1];
+    for (int i = 1; i <= m; i++) {
+        a[i] += a[i-1];
+        b[i] += b[i-1];
+        ret = min(a[i]*(LL)i + b[i], ret);
+    }
+    cout << ret << endl;
 }

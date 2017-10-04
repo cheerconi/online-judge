c5a4c52992a1a3b76aba8125bb8c9fe627223ac6
Carl <cheerconi@163.com>
2017-10-04 21:18:47 +0800

    add Copy & Paste

--- codejam/Kickstart Round E 2017/A. Copy & Paste/main.cpp
+++ codejam/Kickstart Round E 2017/A. Copy & Paste/main.cpp
@@ -1,29 +1,40 @@
 #include <iostream>
 #include <string>
 #include <limits.h>
+#include <algorithm>
+#include <memory.h>
 using namespace std;
+const int MAXN = 100 + 10;
 string s;
-int ret;
+int dp[MAXN][MAXN][MAXN];
 
-void solve(int i, int now, string paste) {
-    if (s.size() == i) {
-        ret = min(ret, now);
-        return;
-    }
-    solve (i+1, now+1, paste);
-    if (paste.size() != 0) {
-        if (s.substr(i, paste.size()) == paste) {
-            solve(i+paste.size(), now+1, paste);
-        }
-    }
-    for (int x = 0; x < i; x++) {
-        for (int len = 2; x+len-1 < i; len++) {
-            paste = s.substr(x, len);
-            if (s.substr(i, len) == paste) {
-                solve(i+len, now+2, paste);
+int solve() {
+    for (int i = 0; i < MAXN; i++) for (int j = 0; j < MAXN; j++) for (int k = 0; k < MAXN; k++) dp[i][j][k] = INT_MAX;
+    dp[0][0][0] = 0;
+    int n = s.size();
+    for (int i = 0; i < n; i++) {
+        dp[i+1][0][0] = min(dp[i+1][0][0], dp[i][0][0]+1);
+        int tmp = dp[i][0][0];
+        for (int j = 0; j < i; j++) for (int k = j+1; k <= i; k++) tmp = min(tmp, dp[i][j][k]);
+        for (int j = 0; j < i; j++) {
+            for (int k = j+1; k <= i; k++) {
+                dp[i][j][k] = min(dp[i][j][k], tmp + 1);
+                dp[i+1][j][k] = min(dp[i+1][j][k], dp[i][j][k] + 1);
+                if (s.substr(j, k-j) == s.substr(i, k-j)) {
+                    dp[i+k-j][j][k] = min(dp[i+k-j][j][k], dp[i][j][k] + 1);
+                }
             }
         }
     }
+    int ret = dp[n][0][0];
+    for (int j = 0; j < n; j++) {
+        for (int k = j+1; k <= n; k++) {
+            ret = min(ret, dp[n][j][k]);
+        }
+    }
+    return ret;
+
+
 }
 
 int main () {
@@ -33,9 +44,7 @@ int main () {
     scanf("%d", &T);
     for (int t = 1; t <= T; t++) {
         cin >> s;
-        ret = INT_MAX;
-        solve(0, 0, "");
-        printf("Case #%d: %d\n", t, ret);
+        printf("Case #%d: %d\n", t, solve());
     }
     return 0;
 

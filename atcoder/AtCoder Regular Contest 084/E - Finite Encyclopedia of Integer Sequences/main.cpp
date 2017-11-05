907289917ba923b808b62450b5db76d77de94355
Carl <cheerconi@163.com>
2017-11-05 20:21:19 +0800

    add Finite Encyclopedia of Integer Sequences

--- atcoder/AtCoder Regular Contest 084/E - Finite Encyclopedia of Integer Sequences/main.cpp
+++ atcoder/AtCoder Regular Contest 084/E - Finite Encyclopedia of Integer Sequences/main.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
+#include <vector>
+using namespace std;
+vector<int> ret;
 
+void solve(int k, int n) {
+    if (n % 2 == 0) {
+        ret.push_back(n/2);
+        for (int i = 0; i < k-1; i++) {
+            ret.push_back(n);
+        }
+        return;
+    }
+    for (int i = 0; i < k; i++) ret.push_back(n/2+1);
+    for (int i = 0; i < k/2; i++) {
+        if (ret.back() == 1) {
+            ret.pop_back();
+            continue;
+        }
+        ret.back()--;
+        while (ret.size() < k) {
+            ret.push_back(n);
+        }
+    }
+
+}
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+//    freopen("test.txt", "r", stdin);
+    int n, k;
+    cin >> k >> n;
+    solve(n, k);
+    for (int i = 0; i < ret.size(); i++) {
+        printf("%d", ret[i]);
+        if (i+1 == ret.size()) printf("\n");
+        else printf(" ");
+    }
 }

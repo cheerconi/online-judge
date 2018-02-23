eeb1a9d3ba525902609967e6f71898a2ef92c1cf
Carl <cheerconi@163.com>
2018-02-24 00:43:43 +0800

    add Tree

--- codeforces/ICM Technex 2018 and Codeforces Round #463/D. Tree/main.cpp
+++ codeforces/ICM Technex 2018 and Codeforces Round #463/D. Tree/main.cpp
@@ -1,53 +1,56 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
 typedef long long LL;
-const int MAXN = 400000 + 10;
-LL val[MAXN][21], sum[MAXN][21];
-int edge[MAXN][21], seq[MAXN][21];
+const int MAXN = 400000  +10;
+int par[MAXN][20];
+LL sum[MAXN][20];
+int cnt = 0;
 
-int find(int cur, LL w) {
-    if (cur == 0 || val[cur][0] >= w) return cur;
-    for (int i = 20; i >= 0; i--) {
-        int tmp = edge[cur][i];
-        if (tmp == 0) continue;
-        if (val[cur][i] < w) return find(edge[edge[cur][i]][0], w);
+void add (int p, LL w) {
+    cnt++;
+    sum[cnt][0] = w;
+    for (int i = 19; i >= 0; i--) {
+        if (sum[par[p][i]][0] < w) p = par[p][i];
+    }
+    if (sum[p][0] < w) p = par[p][0];
+    for (int i = 0; i < 20; i++) {
+        par[cnt][i] = p;
+        if (i+1 < 20) sum[cnt][i+1] = sum[cnt][i] + sum[p][i];
+        p = par[p][i];
     }
 }
-
-void add (int cur, int par, LL w) {
-    edge[cur][0] = par;
-    val[cur][0] = sum[cur][0] = w;
-    for (int i = 1; i <= 20; i++) {
-        int pre = edge[cur][i-1];
-        if (pre == 0) break;
-        val[cur][i] = max(val[cur][i-1], val[pre][i-1]);
-        edge[cur][i] = edge[pre][i-1];
+int query(int cur, LL w) {
+    if (sum[cur][0] > w) return 0;
+    for (int i = 19; i >= 0; i--) {
+        if (sum[cur][i] <= w) {
+            return (1<<i) + query(par[cur][i], w-sum[cur][i]);
+        }
     }
-
-
-
-}
-int query(int cur, LL bound) {
 
 }
 
 int main() {
-    freopen("test.txt", "r", stdin);
+//    freopen("test.txt", "r", stdin);
     ios::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
     int cs; cin >> cs;
-    LL last = 0, p, q, cmd, cnt = 1;
+    for (int i = 0; i < 20; i++) {
+        sum[0][i] = 1e16;
+    }
+    add (0, 0);
+    int cmd;
+    LL p, q, last = 0;
     while (cs--) {
         cin >> cmd >> p >> q;
-        p = p^last;
-        q = q^last;
+        p ^= last;
+        q ^= last;
         if (cmd == 1) {
-            add(++cnt, p, q);
+            add (p, q);
         }
         else {
             last = query(p, q);
             cout << last << '\n';
         }
+
     }
 }
